c215247c982b86ce38c94f6283a6f160984cb5ee
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-01-10 19:06:54 -0600

    this?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '22332app works! testing ng test';
+  title = '2235532app works! testing ng test';
 }
