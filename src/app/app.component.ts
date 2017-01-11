65209f253289c0ec482547986b5c8d6b358da88e
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-01-10 18:54:51 -0600

    testing with chrome installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works! testing ng test';
+  title = '2app works! testing ng test';
 }
